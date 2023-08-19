6e8ea23c9c738e6971f2bdd7c18aeca88c0a4f81
Akash Patil <101398263+akashpatilx7@users.noreply.github.com>
2023-08-19 17:26:23 +0530

    Time: 0 ms (100%), Space: 6.4 MB (13.77%) - LeetHub

--- 1737-maximum-nesting-depth-of-the-parentheses/1737-maximum-nesting-depth-of-the-parentheses.cpp
+++ 1737-maximum-nesting-depth-of-the-parentheses/1737-maximum-nesting-depth-of-the-parentheses.cpp
@@ -1,20 +1,28 @@
 class Solution {
 public:
     int maxDepth(string s) {
-        // Time Complexity : O(1) & Space Complexity : O(1)
+        // Time Complexity : O(N) & Space Complexity : O(N)
 
-        int depth=0, maxDepth=0;
+        int count=0;
+        int maxDepth=INT_MIN;
+        stack<char>st;
 
         for(char c : s){
             if(c=='('){
-                depth++;
-                maxDepth=max(depth,maxDepth);
+                st.push(c); // passing the element to be pushed in the stack to the push function is necessary!
+                count++;
             }
 
             if(c==')'){
-                depth--;
+                st.pop();
+                count=st.size(); // IMP : determining the count from the stack size 
+            }
+
+            if(count>maxDepth){
+            maxDepth=count;
             }
         }
+
         return maxDepth;
     }
 };
