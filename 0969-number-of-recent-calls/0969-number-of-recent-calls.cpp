4b3ff110a46ac93b89a7fa8e2ec7f026f5544ef9
Akash Patil <101398263+akashpatilx7@users.noreply.github.com>
2023-08-25 00:05:59 +0530

    Time: 127 ms (82.44%), Space: 57.3 MB (84.31%) - LeetHub

--- 0969-number-of-recent-calls/0969-number-of-recent-calls.cpp
+++ 0969-number-of-recent-calls/0969-number-of-recent-calls.cpp
@@ -3,20 +3,24 @@ public:
     queue<int>q;
 
     RecentCounter() {
-        // Time Complexity : O(1) & Space Complexity : O(N)
+        // Time Complexity : O(1) & Space Complexity : O(N)    
     }
     
     int ping(int t) {
         if(q.size()==0){
+            // first call
             q.push(t);
             return 1;
         }
 
         else{
             if(t<=3000){
+                // t is in the range of margin
                 q.push(t);
             }
+
             else{
+                // t is not in the range of margin
                 int margin=t-3000;
                 while(q.size()!=0 && q.front()<margin){
                     q.pop();
