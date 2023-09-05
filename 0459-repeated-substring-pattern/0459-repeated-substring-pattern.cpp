f4dbdd64f8c87b3c5265f4fb9964622b56896fc6
Akash Patil <101398263+akashpatilx7@users.noreply.github.com>
2023-09-05 23:48:21 +0530

    Time: 824 ms (6.52%), Space: 11.3 MB (85.37%) - LeetHub

--- 0459-repeated-substring-pattern/0459-repeated-substring-pattern.cpp
+++ 0459-repeated-substring-pattern/0459-repeated-substring-pattern.cpp
@@ -3,15 +3,16 @@ public:
     bool repeatedSubstringPattern(string s) {
         // Time Complexity : O(N) & Space Complexity : O(1)
 
+        // a new string (t) equal to (s), to make changes on & compare to (s) after making changes
         string t=s;
-        for(int i=0;i<s.size()-1;i++){
+        for(int i=0;i<s.length()-1;i++){
             // not reversing the string, only checking if a substring is being repeated or not!
             t.push_back(t[0]);
             t.erase(0,1);
 
             if(s==t){
                 return true;
-            } 
+            }
         }
         return false;
     }
