f1edcb58233f68ec330ab27ee5bcbe0d0535d866
Akash Patil <101398263+akashpatilx7@users.noreply.github.com>
2023-09-08 23:57:33 +0530

    Time: 0 ms (100%), Space: 11.1 MB (20.04%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -4,63 +4,31 @@ public:
         // Time Complexity : O(N) & Space Complexity : O(1)
         
         int n = nums.size();
-        int i = 0;
-        int j = n-1;
+        int left = 0;
+        int right = n - 1;
 
-        while(i<j-1){
-            int mid = (i+j)/2;
-            if (nums[i] <= nums[mid]){
-                if(target>=nums[i] && target <=nums[mid]){
-                    if (nums[mid] == target) 
-                        return mid;
-                    j=mid;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
 
-                    while(i<j-1){
-                        mid = (i+j)/2;
-                        if(nums[mid]<=target){
-                            i=mid;
-                        }
-                        else j = mid;
-                    }
-                    if (nums[i] == target){
-                        return i;
-                    }
-                    else return -1;
-                }
-                else
-                {
-                    i=mid;
-                }
-
+            if (nums[mid] == target) {
+                return mid;
             }
-            else if (nums[mid] <= nums[j])
-            {
-                if(target>=nums[mid] && target<=nums[j])
-                {
-                     if (nums[j] == target) 
-                        return j;
-                    i=mid;
-                    while(i<j-1)
-                    {
-                        mid = (i+j)/2;
-                        if(nums[mid]<=target){
-                            i=mid;
-                        }
-                        else j = mid;
 
-                    }
-                    if (nums[i] == target) return i;
-                    else return -1;
-               }
-               else
-               {
-                    j=mid;
-               }
-
+            if (nums[left] <= nums[mid]) {
+                if (nums[left] <= target && target < nums[mid]) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
+            } else {
+                if (nums[mid] < target && target <= nums[right]) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
             }
         }
-        if (nums[i] == target) return i;
-        if (nums[j] == target) return j;
-        else return -1;
+
+        return -1;
     }
 };
