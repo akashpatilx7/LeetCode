98d479a04bec860f6d813e96608892154af03c30
Akash Patil <101398263+akashpatilx7@users.noreply.github.com>
2023-09-11 00:20:35 +0530

    Time: 3 ms (66.95%), Space: 11.2 MB (20.22%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -2,33 +2,41 @@ class Solution {
 public:
     int search(vector<int>& nums, int target) {
         // Time Complexity : O(N) & Space Complexity : O(1)
-        
-        int n = nums.size();
-        int left = 0;
-        int right = n - 1;
 
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
+        int n=nums.size();
+        int low=0, high=n-1;
 
-            if (nums[mid] == target) {
+        while(low<=high){
+            int mid=low+(high-low)/2;
+
+            if(nums[mid]==target){
                 return mid;
             }
 
-            if (nums[left] <= nums[mid]) {
-                if (nums[left] <= target && target < nums[mid]) {
-                    right = mid - 1;
-                } else {
-                    left = mid + 1;
+            // check if the left half is sorted
+            if(nums[low]<=nums[mid]){
+                // check if the target lies in the left half
+                if(nums[low]<=target && target<=nums[mid]){
+                    high=mid-1;
                 }
-            } else {
-                if (nums[mid] < target && target <= nums[right]) {
-                    left = mid + 1;
-                } else {
-                    right = mid - 1;
+
+                else{
+                    low=mid+1;
+                }
+            }
+
+            // check if the left half is sorted
+            else{
+                // check if the target lies in the right half
+                if(nums[mid]<=target && target<=nums[high]){
+                    low=mid+1;
+                }
+
+                else{
+                    high=mid-1;
                 }
             }
         }
-
         return -1;
     }
 };
