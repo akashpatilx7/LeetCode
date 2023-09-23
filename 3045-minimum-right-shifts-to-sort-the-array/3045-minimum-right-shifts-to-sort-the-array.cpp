c53c86ed8626a2717e7b160ee13b77f562cc44be
Akash Patil <101398263+akashpatilx7@users.noreply.github.com>
2023-09-23 22:48:33 +0530

    Time: 3 ms (92.37%), Space: 23.1 MB (83.6%) - LeetHub

---
+++ 3045-minimum-right-shifts-to-sort-the-array/3045-minimum-right-shifts-to-sort-the-array.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int minimumRightShifts(vector<int>& nums) {
+        // Time Complexity : O(N) & Space Complexity : O(1)
+
+        int n=nums.size();
+        int pivotId=0,pivotCount=0;
+
+        // finding the dropping point (pivotId)
+        for(int i=1;i<n;i++){
+            if(nums[i-1]>nums[i]){
+                pivotId=i;
+                pivotCount++;
+            }
+        }
+
+        // for an array to be sortable after right shifing some elements, there should be exactly 1 dropping point
+        if(pivotCount>1){
+            return -1;
+        }
+
+        if(pivotId==0){
+            return 0;
+        }
+
+        if(nums[n-1]>nums[0]){
+            return -1;
+        }
+
+        else{
+            return n-pivotId;
+        }
+    }
+};
