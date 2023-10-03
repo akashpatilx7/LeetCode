0c0bc11bbc58e4ad63f1ff4cca61918fd4d8985e
Akash Patil <101398263+akashpatilx7@users.noreply.github.com>
2023-10-03 22:56:48 +0530

    Time: 0 ms (100%), Space: 7.5 MB (27.67%) - LeetHub

--- 1635-number-of-good-pairs/1635-number-of-good-pairs.cpp
+++ 1635-number-of-good-pairs/1635-number-of-good-pairs.cpp
@@ -1,19 +1,18 @@
 class Solution {
 public:
     int numIdenticalPairs(vector<int>& nums) {
-        int noOfGoodPairs=0;
+        // Time Complexity : O(N*N) & Space Complexity : O(1)
 
-        for(int i=0;i<nums.size()-1;i++)
-        {
-            for(int j=1;j<nums.size();j++)
-            {
-                if(nums[i]==nums[j] && i<j)
-                {
-                    noOfGoodPairs++;
+        int n=nums.size();
+        int goodPairs=0;
+
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                if(nums[i]==nums[j]){
+                    goodPairs++;
                 }
             }
         }
-
-        return noOfGoodPairs;
+        return goodPairs;
     }
 };
