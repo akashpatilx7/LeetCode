64cca99d5e3909c43db8cf77c70388421d26dd79
Akash Patil <101398263+akashpatilx7@users.noreply.github.com>
2023-10-07 20:17:16 +0530

    Time: 27 ms (10.12%), Space: 21.4 MB (16.99%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
@@ -24,22 +24,24 @@ public:
         }
 
         int leftHeight=maxDepth(root->left);
-        // extra part 
+        // extra part
         if(leftHeight==-1){
             return -1;
         }
 
         int rightHeight=maxDepth(root->right);
-        // extra part 
+        // extra part
         if(rightHeight==-1){
             return -1;
         }
+
+        bool leftPart=isBalanced(root->left);
+        bool rightPart=isBalanced(root->right);
 
         // extra part (condition for balanced binary tree)
         if(abs(leftHeight-rightHeight)>1){
             return -1;
         }
-
         return 1+max(leftHeight,rightHeight);
     }
 };
