8f3586ceda1839b284be485d4d94af211f0eaa7d
Akash Patil <101398263+akashpatilx7@users.noreply.github.com>
2023-10-07 09:57:59 +0530

    Time: 12 ms (41%), Space: 21.4 MB (20.97%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
@@ -11,40 +11,35 @@
  */
 class Solution {
 public:
+    bool isBalanced(TreeNode* root) {
+        // Time Complexity : O(N) & Space Complexity : O(N)
+
+        // if the tree is balanced, return its height
+        return maxDepth(root)!=-1;
+    }
+
     int maxDepth(TreeNode* root){
         if(root==NULL){
             return 0;
         }
 
         int leftHeight=maxDepth(root->left);
+        // extra part 
+        if(leftHeight==-1){
+            return -1;
+        }
+
         int rightHeight=maxDepth(root->right);
+        // extra part 
+        if(rightHeight==-1){
+            return -1;
+        }
+
+        // extra part (condition for balanced binary tree)
+        if(abs(leftHeight-rightHeight)>1){
+            return -1;
+        }
 
         return 1+max(leftHeight,rightHeight);
-    }
-
-    bool isBalanced(TreeNode* root) {
-        // Time Complexity : O(N*N) & Space Complexity : O(N)
-
-        if(root==NULL){
-            return true;
-        }
-
-        // calculate the left & right height for checking balanced binary tree
-        int leftHeight=maxDepth(root->left);
-        int rightHeight=maxDepth(root->right);
-
-        // condition for balanced binary tree
-        if(abs(leftHeight-rightHeight)>1){
-            return false;
-        }
-
-        // check if the left & right parts are balanced
-        bool leftPart=isBalanced(root->left);
-        bool rightPart=isBalanced(root->right);
-
-        if(!leftPart || !rightPart){
-            return false;
-        }
-        return true;
     }
 };
