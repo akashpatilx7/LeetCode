3a9d28a8a8ad2a67793f082504e7ff0003aa6bf2
Akash Patil <101398263+akashpatilx7@users.noreply.github.com>
2023-10-12 09:17:27 +0530

    Time: 11 ms (76.98%), Space: 10.7 MB (39.73%) - LeetHub

--- 1476-count-negative-numbers-in-a-sorted-matrix/1476-count-negative-numbers-in-a-sorted-matrix.cpp
+++ 1476-count-negative-numbers-in-a-sorted-matrix/1476-count-negative-numbers-in-a-sorted-matrix.cpp
@@ -1,20 +1,24 @@
 class Solution {
 public:
     int countNegatives(vector<vector<int>>& grid) {
-        // Time Complexity :O(N*N) & Space Complexity : O(1) 
+        // Time Complexity : O(M+N) & Space Complexity : O(1) ----> TWO POINTER APPROACH 
 
-        int i,j;
+        int i;
+        int count=0;
         int m=grid.size();
         int n=grid[i].size();
-        int ans=0;
+        int row=m-1, col=0;
+        
+        while(row>=0 && col<n){
+            if(grid[row][col]<0){
+                row--;
+                count+=n-col;
+            }
 
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(grid[i][j]<0){
-                    ans++;
-                }
+            else{
+                col++;
             }
         }
-        return ans;
+        return count;
     }
 };
