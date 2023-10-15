69b48cbb23cf9137122345f26760816cb561e155
Akash Patil <101398263+akashpatilx7@users.noreply.github.com>
2023-10-15 09:16:51 +0530

    Time: 3 ms (84.55%), Space: 15.9 MB (6.22%) - LeetHub

--- 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.cpp
+++ 0747-min-cost-climbing-stairs/0747-min-cost-climbing-stairs.cpp
@@ -1,37 +1,29 @@
 class Solution {
 public:
     int minCostClimbingStairs1(vector<int>& cost, int index, vector<int>& memo){
-        // Time Complexity : O(N) & Space Complexity : O(N)
-
-        // base case 
         if(index==cost.size()-1 || index==cost.size()-2){
             return cost[index];
         }
 
-        // use of dp
         if(memo[index]!=0){
             return memo[index];
         }
 
-        // two possible calls at each current index
         int x=minCostClimbingStairs1(cost,index+1,memo);
         int y=minCostClimbingStairs1(cost,index+2,memo);
 
-        // calculate memo of current index, if not already calculated
         memo[index]=min(x,y)+cost[index];
         return memo[index];
     }
 
     int minCostClimbingStairs(vector<int>& cost) {
-        // edge case
+        // edge case 
         if(cost.size()==2){
-            return min(cost[0],cost[1]);
+            return min(cost[0],cost[1]); 
         }
 
-        // memoization
         vector<int>memo(1001);
-        
-        // deciding the starting index
+
         int x=minCostClimbingStairs1(cost,0,memo);
         int y=minCostClimbingStairs1(cost,1,memo);
 
