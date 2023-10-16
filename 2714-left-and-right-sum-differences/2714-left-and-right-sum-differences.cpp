d2e44fa91be285b1ebe2058a42fa8ed3e5989490
Akash Patil <101398263+akashpatilx7@users.noreply.github.com>
2023-10-16 18:08:54 +0530

    Time: 5 ms (69.78%), Space: 11.9 MB (9.8%) - LeetHub

---
+++ 2714-left-and-right-sum-differences/2714-left-and-right-sum-differences.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    vector<int> leftRightDifference(vector<int>& nums) {
+        // Time Complexity : O(N) & Space Complexity : O(N)
+
+        vector<int>answer;
+        int n=nums.size();
+        int sum=0;
+
+        // 1. calculate the leftSum
+        vector<int>leftSum;
+        for(int i=0;i<n;i++){
+            leftSum.push_back(sum);
+            sum+=nums[i];
+        }
+
+        // 2. calculate the rightSum
+        sum=0;
+        vector<int>rightSum(n,0);
+        for(int i=n-1;i>=0;i--){
+            rightSum[i]=sum;
+            sum+=nums[i];
+        }
+
+        // 3. calculate the absolute difference between the both
+        for(int i=0;i<n;i++){
+            answer.push_back(abs(leftSum[i]-rightSum[i]));
+        }
+        return answer;
+    }
+};
