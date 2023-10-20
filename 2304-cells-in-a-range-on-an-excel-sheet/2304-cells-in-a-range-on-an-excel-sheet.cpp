f97d02bd3d2614780895ecc805ac12241dc39b29
Akash Patil <101398263+akashpatilx7@users.noreply.github.com>
2023-10-20 12:35:04 +0530

    Time: 5 ms (32.49%), Space: 8.2 MB (17.25%) - LeetHub

--- 2304-cells-in-a-range-on-an-excel-sheet/2304-cells-in-a-range-on-an-excel-sheet.cpp
+++ 2304-cells-in-a-range-on-an-excel-sheet/2304-cells-in-a-range-on-an-excel-sheet.cpp
@@ -2,19 +2,19 @@ class Solution {
 public:
     vector<string> cellsInRange(string s) {
         // Time Complexity : O(N) & Space Complexity : O(N)
-        
-        vector<string>ans;
+
+        vector<string>ans; // imp
         char col1=s[0], row1=s[1], col2=s[3], row2=s[4];
 
         for(int i=0;i+col1<=col2;i++){
             for(int j=0;j+row1<=row2;j++){
                 string res;
-                res+=(col1+i);
-                res+=(row1+j);
+                res+=col1+i; // adds column in string res
+                res+=row1+j; // adds row in string res
+                // 1 cell is ready, push back in ans
                 ans.push_back(res);
             }
         }
         return ans;
-
     }
 };
