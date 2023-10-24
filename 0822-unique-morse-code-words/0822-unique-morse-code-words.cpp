5752d3e15570f509c72af0d00d9bb77b25e6f596
Akash Patil <101398263+akashpatilx7@users.noreply.github.com>
2023-10-24 09:00:28 +0530

    Time: 0 ms (100%), Space: 8.7 MB (62.29%) - LeetHub

--- 0822-unique-morse-code-words/0822-unique-morse-code-words.cpp
+++ 0822-unique-morse-code-words/0822-unique-morse-code-words.cpp
@@ -1,16 +1,12 @@
 class Solution {
 public:
     int uniqueMorseRepresentations(vector<string>& words) {
-        // Time Complexity : O(N) & Space Complexity : O(N)
-
         string morseCodes[27]={".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."};
 
-        // to store only the unique transformations formed
         unordered_set<string>uset;
 
         for(int i=0;i<words.size();i++){
             string curr="";
-            // whenever you don't know the size of the string/ array to be traversed, use "for loop using auto"! 
             for(auto& ch:words[i]){
                 curr+=morseCodes[ch-'a'];
             }
