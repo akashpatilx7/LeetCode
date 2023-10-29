1cd865b95f0d5bc21e918a1fcc2c1d3d428a4c9c
Akash Patil <101398263+akashpatilx7@users.noreply.github.com>
2023-10-29 08:50:20 +0530

    Time: 14 ms (83.17%), Space: 23.3 MB (73.3%) - LeetHub

--- 0344-reverse-string/0344-reverse-string.cpp
+++ 0344-reverse-string/0344-reverse-string.cpp
@@ -3,12 +3,21 @@ public:
     void reverseString(vector<char>& s) {
         // Time Complexity : O(N) & Space Complexity : O(1)
 
-        int l=0, r=s.size()-1;
+        // The most suitable Algorithm to Reverse a String is "TWO-POINTER" Algorithm.
 
-        while(l<r){
-            swap(s[l],s[r]);
-            l++;
-            r--;
+        int left=0;
+        int right=s.size()-1;
+        char temp;
+
+        while(left<right)
+        // Even if we take (left<=right), the swapping of the Middle Element takes place with itself. So, no difference in the string will occur.
+        {
+            temp=s[left];
+            s[left]=s[right];
+            s[right]=temp;
+
+            left++;
+            right--;
         }
     }
 };
